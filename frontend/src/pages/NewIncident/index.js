bd4c33f4e0be57fadbed33e3dd3e98693a696238
joaovitor32 <joaovitormunizlopes@gmail.com>
2020-03-28 18:22:54 -0300

    Modificações no Frontend

--- frontend/src/pages/NewIncident/index.js
+++ frontend/src/pages/NewIncident/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 
 import './style.css'
-import { Link ,useHistory} from 'react-router-dom'
+import { Link, useHistory } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
 
 import logoImg from '../../assets/logo.svg'
@@ -10,30 +10,30 @@ import api from '../../services/api'
 
 
 const NewIncident = props => {
+    
+    const ong_id = localStorage.getItem('ongId')
+    const [title, setTitle] = useState('')
+    const [description, setDescription] = useState('')
+    const [value, setValue] = useState('')
+    const history = useHistory()
 
-    const ong_id=localStorage.getItem('ongInd')
-    const [title,setTitle]=useState('')
-    const [description,setDescription]=useState('')
-    const [value,setValue]=useState('')
-    const history=useHistory()
-
-    const handleNewIncident =async e=>{
+    const handleNewIncident = async e => {
         e.preventDefault();
-        
-        const data={
+
+        const data = {
             title,
             description,
             value
         }
-
-        try{
-            await api.post('incidents',data,{
+        console.log(data)
+        try {
+            await api.post('incidents', data, {
                 headers:{
-                    authorization:ong_id
+                    Authorization:ong_id
                 }
             })
             history.push('/profile')
-        }catch(err){
+        } catch (err) {
             alert('erro ao cadastrar caso')
         }
     }
@@ -49,13 +49,13 @@ const NewIncident = props => {
                     <FiArrowLeft size={16} color="#E02041" />Voltar para home</Link>
             </section>
             <form onSubmit={handleNewIncident}>
-                <input  
-                    onChange={e=>setTitle(e.target.value)} value={title} placeholder="Título do caso..." 
-                 />
-                <textarea   
-                    onChange={e=>setDescription(e.target.value)} value={description} placeholder="Descrição"></textarea>
-                <input  
-                    onChange={e=>setValue(e.target.value)} value={value} placeholder="Valor" />
+                <input
+                    onChange={e => setTitle(e.target.value)} value={title} placeholder="Título do caso..."
+                />
+                <textarea
+                    onChange={e => setDescription(e.target.value)} value={description} placeholder="Descrição"></textarea>
+                <input
+                    onChange={e => setValue(e.target.value)} value={value} placeholder="Valor" />
                 <button className="button" type="submit ">Cadastrar</button>
             </form>
         </div>
